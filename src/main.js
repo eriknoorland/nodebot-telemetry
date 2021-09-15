d4be2c26ddc0e3ec9fce1befcfee66ca23100652
Erik Noorland <mail@eriknoorland.nl>
2021-09-15 18:41:28 +0200

    Fix socket.io CORS issue, Remove production env file

--- src/main.js
+++ src/main.js
@@ -1,14 +1,16 @@
 import Vue from 'vue';
 import VueSocketIO from 'vue-socket.io';
+import SocketIO from 'socket.io-client';
 import App from './App.vue';
 import router from './router';
 import store from './store';
 
+const socketConnection = SocketIO(process.env.VUE_APP_SOCKETIO_HOST);
 const defaultArena = store.state.setup.arenas[0];
 const selectedArena = window.localStorage.getItem('selected_arena');
 
 Vue.use(new VueSocketIO({
-  connection: process.env.VUE_APP_SOCKETIO_HOST,
+  connection: socketConnection,
   vuex: {
     store,
     actionPrefix: 'SOCKET_',
